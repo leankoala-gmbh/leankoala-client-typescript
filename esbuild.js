f01ade34678f07b80b0080d56417685c7e6dbc6e
saschafuchs <sascha.fuchs@webpros.com>
2022-05-18 12:39:08 +0200

    Refine Build

--- esbuild.js
+++ esbuild.js
@@ -1,6 +1,5 @@
 const { build } = require ('esbuild')
 const { dependencies } = require ('./package.json')
-const { Generator } = require('npm-dts')
 
 const shared = {
   entryPoints: ['./src/360ApiClient'],
@@ -8,18 +7,6 @@ const shared = {
   sourcemap: true,
   external: Object.keys( dependencies )
 }
-
-const sharedSingleFiles = {
-  minify: true,
-  format: 'esm',
-  target: ['es6']
-}
-
-
-// new Generator({
-//   entry: './src/360ApiClient.ts',
-//   output: './dist/types/360ApiClient.d.ts'
-// }).generate()
 
 build({
   ...shared,
@@ -33,16 +20,4 @@ build({
   format: 'esm',
   outfile: './dist/360ApiClient.esm.js',
   target: ['es6'],
-})
-
-build({
-  ...sharedSingleFiles,
-  entryPoints: ['./src/Connection/RefreshTokenInvalidError.ts', './src/Connection/BadRequestError.ts'],
-  outdir: './dist/connection'
-})
-
-build({
-  ...sharedSingleFiles,
-  entryPoints: ['./src/Repository/Constants/Marketplace.ts'],
-  outdir: './dist/constants'
 })
