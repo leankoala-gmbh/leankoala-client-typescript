5b44bffd9949a9ff678fdc9759fdffc6d9d57dc9
saschafuchs <sascha.fuchs@webpros.com>
2023-02-06 12:39:08 +0100

    fix: switch domain for endpoint

--- src/Connection/SessionConnector.ts
+++ src/Connection/SessionConnector.ts
@@ -106,7 +106,7 @@ class SessionConnector {
         default:
           throw new Error('The given environment "' + this.environment + '" is unknown.')
       }
-    } else if (domain.includes('360monitoring.com')) {
+    } else if (domain.includes('koality.360monitoring.com')) {
       return `https://${['auth', ...domain.split('.').slice(1)].join('.')}${path}`
 
     } else {
