ae5eb79ca621ea76bfeaa2f97ef2d2ef0b9312b7
saschafuchs <sascha.fuchs@webpros.com>
2023-05-08 11:22:21 +0200

    fix: add parameter for system size call

--- src/Repository/Entities/SystemRepository.ts
+++ src/Repository/Entities/SystemRepository.ts
@@ -154,10 +154,11 @@ class SystemRepository extends Repository {
    *
    * @param providerIdentifier
    * @param {Object} args
+   * @param system_size_id The system size id. (optional)
    */
-  async getSystemTypes(providerIdentifier): Promise<any> {
+  async getSystemTypes(providerIdentifier, system_size_id): Promise<any> {
     const route = { path: 'project/systems/{providerIdentifier}/systemType', method: 'POST', version: 1 }
-    const argList = Object.assign({ providerIdentifier }, {})
+    const argList = Object.assign({ providerIdentifier, system_size_id }, {})
 
     return this.connection.send(route, argList)
   }
